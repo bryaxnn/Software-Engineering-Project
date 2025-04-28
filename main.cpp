04404a3ea1247b99d63fe6c8902a22061f67e55d
bryaxnn <brymart27@gmail.com>
2025-04-28 00:56:00 -0500

    Part of Librarian Account Code
    I have to work on the other 2. will prolly get to it when we meet tomorrow, feel free to let me know what's wrong, there's def some errors I bet

--- main.cpp
+++ main.cpp
@@ -34,6 +34,71 @@ int main() {
 
     //Check account type to determine what actions to give access to
     int accountType = accounts.getAccountType(userName);
+
+    if (accountType == "lib") // Librarian Account Choice 
+{
+    int choice = 0;
+    while (choice != 5) // Options when accessing Librarian Account.
+    {
+        cout << "Librarian Menu:" << endl;
+        cout << "1. Add Book" << endl;
+        cout << "2. Check Out Book" << endl;
+        cout << "3. Check In Book" << endl;
+        cout << "4. View Inventory" << endl;
+        cout << "5. Logout" << endl;
+        cin >> choice;
+        cin.ignore();
+
+    if (choice == 1) // If statement used to add a book.
+    {
+        string title, author;
+        cout << "Enter Book Title: ";
+        cin.ignore();
+        getline(cin, title);
+        cout << "Enter Book Author: ";
+        getline(cin, author);
+        inventory.Append(title, author);
+        cout << "Book added!" << endl;
+    }
+    else if (choice == 2) // else if statement used to check out a book.
+    {
+        string title;
+        cout << "Enter Book Title to Check Out: ";
+        cin.ignore();
+        getline(cin, title);
+        
+        if (inventory.foundBook(title)) 
+        {
+            inventory.changeStatus(title);
+            cout << "Book checked out!" << endl;
+        }
+
+        else 
+        {
+            cout << "Book not found." << endl;
+        }
+    }
+    else if (choice == 3) // used to check in a book
+    {
+        string title;
+        cout << "Enter Book Title to Check In: ";
+        cin.ignore();
+       getline(cin, title);
+       
+       if (inventory.foundBook(title)) 
+       {
+            inventory.changeStatus(title);
+            cout << "Book checked in!" << endl;
+        }
+
+        else 
+       {
+            cout << "Book not found." << endl;
+        }
+    }
+
+    // still gotta work on the other 2, just adding what I have.
+}
     
     //UI Function Selections
 }
